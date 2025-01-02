ffcc6ccaf4af904cb85252fed9bec1c869d15d10
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2025-01-02 21:35:41 +0530

    update user model

--- src/main/java/com/cinnmon/user_service/model/User.java
+++ src/main/java/com/cinnmon/user_service/model/User.java
@@ -1,6 +1,12 @@
 package com.cinnmon.user_service.model;
 
+import lombok.*;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class User {
     private String name;
     private String email;
