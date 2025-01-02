58ccad2b4371573176082f764224dceb0009f731
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2025-01-02 21:56:23 +0530

    create UserRepository and add in findByEmail method

---
+++ src/main/java/com/cinnmon/user_service/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.cinnmon.user_service.repository;
+
+import com.cinnmon.user_service.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+}
